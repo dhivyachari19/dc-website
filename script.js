798cc3bb18a630bf140ea2412b832eda34b2faf8
dhivyachari19 <dhivyac2@illinois.edu>
2024-06-13 11:31:31 -0500

    mobile nav bar

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+window.onload = function() {
+    setupNavigationBar();
+};
+
 window.onscroll = function() {
     const nav = document.querySelector('.navbar');
     if(this.scrollY <= 75) {
@@ -6,3 +10,33 @@ window.onscroll = function() {
         nav.className = 'navbar fixed-top navbar-expand-lg navbar-dark bg-dark';
     }
 };
+
+function detectMob() {
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ];
+    
+    return toMatch.some((toMatchItem) => {
+        return navigator.userAgent.match(toMatchItem);
+    });
+}
+
+function setupNavigationBar() {
+    const isMobile = detectMob();
+    const mobileNav = document.querySelector('.pos-f-t');
+    const desktopNav = document.querySelector('.navbar');
+
+    if (isMobile) {
+        desktopNav.style.display = 'none'; 
+        mobileNav.style.display = 'block'; 
+    } else {
+        desktopNav.style.display = 'block';
+        mobileNav.style.display = 'none'; 
+    }
+}
